28969a9359b5e08f12d34ec4cad1e54eba7177b3
gjiyaer <georgijguliev6@gmail.com>
2023-09-15 14:37:55 +0300

    1.6 Аргументы.

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -11,13 +11,13 @@ public class Calculator {
         int  age = 6;
         Calculator.hello(name, age);
         Calculator.hello(name, age);
-        Calculator.hello(String.valueOf(age), age);
-        Calculator.hello(String.valueOf(age), name);
         Calculator.hello(name, age);
+        Calculator.hello(name, age);
+
+
     }
 
-    private static void hello(String name, String name1) {
-    }
+
 
 
 }
