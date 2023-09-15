e52fd5d8b509ef904eac375e01626c802e6740c5
gjiyaer <georgijguliev6@gmail.com>
2023-09-15 09:33:52 +0300

    1.6 Аргументы.

--- src/main/java/ru/calculator/Calculator.java
+++ src/main/java/ru/calculator/Calculator.java
@@ -1,21 +1,23 @@
 package ru.calculator;
 
-public class Calculator{
+public class Calculator {
+
+    public static void hello(String name, int age) {
+        System.out.println("Hello, " + name + ", age = " + age);
+    }
 
     public static void main(String[] args) {
-        int one = 1;
-        int two = 2;
-        int four = 4;
-        int six = 6;
-        int five = 5;
-        int onePlusTwo = one + two;
-        System.out.println(onePlusTwo);
-        int sixDivTwo = six / two;
-        System.out.println(sixDivTwo);
-        int fiveMinusTwo = five - two;
-        System.out.println(fiveMinusTwo);
-        int fourTimeTwo = four * two;
-        System.out.println(fourTimeTwo);
-
+        String name = "Java";
+        int  age = 6;
+        Calculator.hello(name, age);
+        Calculator.hello(name, age);
+        Calculator.hello(String.valueOf(age), age);
+        Calculator.hello(String.valueOf(age), name);
+        Calculator.hello(name, age);
     }
+
+    private static void hello(String name, String name1) {
+    }
+
+
 }
